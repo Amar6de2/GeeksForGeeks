95417d5d9a9725845a06747b5a3a2555872db524
Amarnath Tiwari <134729694+Amar6de2@users.noreply.github.com>
2025-07-30 23:48:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -1,63 +1,39 @@
-//{ Driver Code Starts
-#include <stdio.h>
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+  public:
 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to sort an array using quick sort algorithm.
-    void quickSort(int arr[], int low, int high)
-    {
+    void quickSort(vector<int>& arr, int low, int high) {
         // code here
-        if(low>=high){return;}//one element is always sorted
-        int part=partition(arr,low,high);
-        quickSort(arr,low,part-1);
-        quickSort(arr,part+1,high);
+        if(low>=high){return;}
+        int partt=partition(arr,low,high);
+        quickSort(arr,low,partt-1);
+        quickSort(arr,partt+1,high);
     }
-    
-    public:
-    int partition (int arr[], int low, int high)
-    {
-       // Your code here
-       int i=low;
-       int j=high;
-       int pivot=arr[low];
-       while(j>i)
-       {
-           while(i<=(high-1) && pivot>=arr[i]){i++;}
-           while(j>=(low+1) && pivot<arr[j]){j--;}
-           if(i<j){swap(arr[i],arr[j]);}
-       }
-       swap(arr[j],arr[low]);
-       return j;
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        
+        // code here
+        
+        int pivot=arr[low];
+        int i=low;
+        int j=high;
+        while(j>i)// = daalne se TLE !!
+        {
+            while( i<high && pivot>=arr[i])
+            {
+                i++;
+            }
+            while(pivot<arr[j] && j>low)//mistake here !!
+            {
+                j--;
+            }
+            if(i<j)
+            {swap(arr[i],arr[j]);
+               
+            }
+        }
+        swap(arr[low],arr[j]);
+        return j;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-int main()
-{
-    int arr[1000],n,T,i;
-    scanf("%d",&T);
-    while(T--){
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      Solution ob;
-    ob.quickSort(arr, 0, n-1);
-    printArray(arr, n);
-    }
-    return 0;
-}
-// } Driver Code Ends
